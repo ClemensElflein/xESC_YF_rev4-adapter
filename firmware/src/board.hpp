2607d91cf858bd5a933c73010ae8855e43574698
Jörg Ebeling <joerg@ebeling.ws>
2024-07-18 12:01:29 +0200

    Adapt to newest modm changes

--- firmware/src/board.hpp
+++ firmware/src/board.hpp
@@ -1,21 +1,25 @@
-/*
- * Copyright (c) 2019, Niklas Hauser
- *
- * This file is part of the modm project.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
+// Created by Apehaenger on 2024-06-14.
+//
+// This file is part of the openmower project.
+//
+// This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+//
+// Feel free to use the design in your private/educational projects, but don't try to sell the design or products based on it without getting my consent first.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
 
-#ifndef MODM_STM32_CUSTOM_STM32C0X_HPP
-#define MODM_STM32_CUSTOM_STM32C0X_HPP
+#ifndef MODM_XESCYFR4_BOARD_HPP
+#define MODM_XESCYFR4_BOARD_HPP
 
 #include <modm/architecture/interface/clock.hpp>
 #include <modm/platform.hpp>
-// #include <modm/debug/logger.hpp>
-/// @ingroup modm_board_custom_stm32c0x modm_board_custom_stm32c0x
-// #define MODM_BOARD_HAS_LOGGER
 
 using namespace modm::platform;
 
@@ -24,75 +28,30 @@ namespace Board {
 /// @{
 using namespace modm::literals;
 
-/// STM32C011 running at 48MHz generated from internal RC
 struct SystemClock {
-    static constexpr uint32_t Frequency = 48_MHz;
+    static constexpr uint32_t Frequency = 48_MHz;  // 48MHz generated from internal RC
     static constexpr uint32_t Ahb = Frequency;
     static constexpr uint32_t Apb = Frequency;
 
-    static constexpr uint32_t Aes = Ahb;
-    static constexpr uint32_t Rng = Ahb;
     static constexpr uint32_t Crc = Ahb;
     static constexpr uint32_t Flash = Ahb;
     static constexpr uint32_t Exti = Ahb;
     static constexpr uint32_t Rcc = Ahb;
-    static constexpr uint32_t Dmamux = Ahb;
-    static constexpr uint32_t Dma = Ahb;
-
-    static constexpr uint32_t Dbg = Apb;
-    static constexpr uint32_t Timer17 = Apb;
-    static constexpr uint32_t Timer16 = Apb;
-    static constexpr uint32_t Timer15 = Apb;
-    static constexpr uint32_t Usart1 = Apb;
-    static constexpr uint32_t Spi1 = Apb;
-    static constexpr uint32_t I2s1 = Apb;
     static constexpr uint32_t Timer1 = Apb;
-    static constexpr uint32_t Adc1 = Apb;
-    static constexpr uint32_t Comp = Apb;
-    static constexpr uint32_t ItLine = Apb;
-    static constexpr uint32_t VrefBuf = Apb;
-    static constexpr uint32_t SysCfg = Apb;
-    static constexpr uint32_t Tamp = Apb;
-    static constexpr uint32_t Bkp = Apb;
-    static constexpr uint32_t Ucpd2 = Apb;
-    static constexpr uint32_t Ucpd1 = Apb;
-    static constexpr uint32_t LpTimer2 = Apb;
-    static constexpr uint32_t LpUart1 = Apb;
-    static constexpr uint32_t LpTimer1 = Apb;
-    static constexpr uint32_t HdmiCec = Apb;
-    static constexpr uint32_t Dac = Apb;
-    static constexpr uint32_t Pwr = Apb;
-    static constexpr uint32_t I2c2 = Apb;
-    static constexpr uint32_t I2c1 = Apb;
-    static constexpr uint32_t Usart4 = Apb;
-    static constexpr uint32_t Usart3 = Apb;
-    static constexpr uint32_t Usart2 = Apb;
-    static constexpr uint32_t Spi2 = Apb;
-    static constexpr uint32_t Iwdg = Rcc::LsiFrequency;
-    static constexpr uint32_t Wwdg = Apb;
-    static constexpr uint32_t Rtc = Apb;
-    static constexpr uint32_t Timer14 = Apb;
-    static constexpr uint32_t Timer7 = Apb;
-    static constexpr uint32_t Timer6 = Apb;
     static constexpr uint32_t Timer3 = Apb;
-    static constexpr uint32_t Timer2 = Apb;
+    static constexpr uint32_t Timer14 = Apb;
+    static constexpr uint32_t Timer16 = Apb;
+    static constexpr uint32_t Timer17 = Apb;
+    static constexpr uint32_t Usart1 = Ahb;
+    static constexpr uint32_t Usart2 = Ahb;
 
     static bool inline enable() {
-        Rcc::enableInternalClock();  // 48MHz
-        /* // (internal clock / 1_M) * 8_N / 2_R = 128MHz / 2 = 64MHz
-        const Rcc::PllFactors pllFactors{
-                .pllM = 1,
-                .pllN = 8,
-                .pllR = 2,
-        };
-        Rcc::enablePll(Rcc::PllSource::InternalClock, pllFactors); */
+        Rcc::enableInternalClock();                       // 48MHz generated from internal RC
+        Rcc::setHsiSysDivider(Rcc::HsiSysDivider::Div1);  // = 48MHz HSISYS
         Rcc::setFlashLatency<Frequency>();
-        // switch system clock to PLL output
-        // Rcc::enableSystemClock(Rcc::SystemClockSource::Pll);
-        Rcc::setAhbPrescaler(Rcc::AhbPrescaler::Div1);
-        Rcc::setApbPrescaler(Rcc::ApbPrescaler::Div1);
-        // update frequencies for busy-wait delay functions
-        Rcc::updateCoreFrequency<Frequency>();
+        Rcc::setAhbPrescaler(Rcc::AhbPrescaler::Div1);  // = 48MHz HCLK
+        Rcc::setApbPrescaler(Rcc::ApbPrescaler::Div1);  // = 48MHz PCLK/APB Timer Clocks
+        Rcc::updateCoreFrequency<Frequency>();          // update frequencies for busy-wait delay functions
 
         return true;
     }
@@ -104,33 +63,37 @@ using LedRd = GpioC14;
 using Leds = SoftwareGpioPort<LedGn, LedRd>;
 /// @}
 
-/*namespace stlink
-{
-/// @ingroup modm_board_custom_stm32c0x modm_board_custom_stm32c0x
+namespace om_uart {
+/// @ingroup modm_board_custom_stm32c0x
 /// @{
-using Rx = GpioInputA3;
-using Tx = GpioOutputA2;
-using Uart = BufferedUart<UsartHal2, UartTxBuffer<2048>>;
+using Tx = GpioOutputA9;
+using Rx = GpioInputA10;
+using Uart = BufferedUart<UsartHal1, UartTxBuffer<64>>;
 /// @}
-}*/
+}  // namespace om_uart
 
-/// @ingroup modm_board_custom_stm32c0x modm_board_custom_stm32c0x
+namespace proto_uart {
+/// @ingroup modm_board_custom_stm32c0x
 /// @{
-// using LoggerDevice = modm::IODeviceWrapper< stlink::Uart, modm::IOBuffer::BlockIfFull >;
+using Tx = GpioOutputA4;
+using Rx = GpioInputA5;
+using Uart = BufferedUart<UsartHal2, UartTxBuffer<64>>;
+/// @}
+}  // namespace proto_uart
 
 inline void
 initialize() {
     SystemClock::enable();
     SysTickTimer::initialize<SystemClock>();
 
-    // stlink::Uart::connect<stlink::Tx::Tx, stlink::Rx::Rx>();
-    // stlink::Uart::initialize<SystemClock, 115200_Bd>();
+    // Remap om_uart GPIOs
+    GpioA9::remap();   // Remap A9 -> A11
+    GpioA10::remap();  // Remap A10 -> A12
 
     LedGn::setOutput();
     LedRd::setOutput();
 }
-/// @}
 
 }  // namespace Board
 
-#endif  // MODM_STM32_CUSTOM_STM32C0X_HPP
+#endif  // MODM_XESCYFR4_BOARD_HPP
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,15 +1,80 @@
-#include <modm/platform.hpp>
-#include "board.hpp"
+// Created by Apehaenger on 2024-06-14.
+//
+// This file is part of the openmower project.
+//
+// This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+//
+// Feel free to use the design in your private/educational projects, but don't try to sell the design or products based on it without getting my consent first.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
 
-using namespace modm::platform;
+#include <modm/debug/logger.hpp>
+#include <modm/platform.hpp>
+
+#include "board.hpp"
+//#include "LED.h"
+
+using namespace Board;
 using namespace std::chrono_literals;
+
+// --- UART Debugging ---
+#undef MODM_LOG_LEVEL
+#define MODM_LOG_LEVEL modm::log::DEBUG
+// Create an IODeviceWrapper around the Uart Peripheral we want to use
+modm::IODeviceWrapper<proto_uart::Uart, modm::IOBuffer::BlockIfFull> loggerDevice;
+// Set all four logger streams to use the UART
+modm::log::Logger modm::log::debug(loggerDevice);
+modm::log::Logger modm::log::info(loggerDevice);
+modm::log::Logger modm::log::warning(loggerDevice);
+modm::log::Logger modm::log::error(loggerDevice);
+
+//LED led_green(PIN_LED_GREEN);
+//LED led_red(PIN_LED_RED);
+
+MODM_ISR(TIM14) {
+    Timer14::acknowledgeInterruptFlags(Timer14::InterruptFlag::Update);
+    MODM_LOG_DEBUG << "Toggle green LED" << modm::endl;
+    LedGn::toggle();
+}
+
+MODM_ISR(TIM16) {
+    Timer16::acknowledgeInterruptFlags(Timer16::InterruptFlag::Update);
+    MODM_LOG_DEBUG << "Toggle red LED" << modm::endl;
+    LedRd::toggle();
+}
 
 int main() {
     Board::initialize();
-    Board::LedGn::set();
+
+    // Initialize prototyping-UART for MODM_LOG_*
+    proto_uart::Uart::connect<proto_uart::Tx::Tx, proto_uart::Rx::Rx>();
+    proto_uart::Uart::initialize<SystemClock, 460800_Bd>();
+
+    MODM_LOG_INFO << "Board & Logger initialized" << modm::endl;
+
+    Timer14::enable();
+    Timer14::setMode(Timer14::Mode::UpCounter);
+    Timer14::setPeriod<Board::SystemClock>(2000ms);
+    Timer14::applyAndReset();
+    Timer14::start();
+    Timer14::enableInterrupt(Timer14::Interrupt::Update);
+    Timer14::enableInterruptVector(true, 5);
+
+    Timer16::enable();
+    Timer16::setMode(Timer16::Mode::UpCounter);
+    Timer16::setPeriod<Board::SystemClock>(250ms);
+    Timer16::applyAndReset();
+    Timer16::start();
+    Timer16::enableInterrupt(Timer16::Interrupt::Update);
+    Timer16::enableInterruptVector(true, 5);
+
     while (true) {
-        Board::LedGn::toggle();
-        Board::LedRd::toggle();
-        modm::delay(0.5s);
     }
 }
