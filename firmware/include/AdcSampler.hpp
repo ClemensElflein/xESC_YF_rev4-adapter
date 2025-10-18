b8290bbd4c99eeaeb36ecd12a66926abe1c74a73
Jörg Ebeling <joerg@ebeling.ws>
2025-10-19 00:07:33 +0200

    Formatting

--- firmware/include/AdcSampler.hpp
+++ firmware/include/AdcSampler.hpp
@@ -29,127 +29,122 @@
 using namespace modm::literals;
 
 class AdcSampler : public modm::platform::Adc1 {
-public:
-    enum Sensors : uint8_t {
-        VRef = 0,
-        CurSense,   // = In2
-        Temp,
-    };
+ public:
+  enum Sensors : uint8_t {
+    VRef = 0,
+    CurSense,  // = In2
+    Temp,
+  };
 
-    // Has to match Sensors order. FIXME: Somehow static
-    static constexpr std::array<Channel, 3> sequence{
-        Channel::InternalReference,
-        Board::CurSenseChan,
-        Channel::Temperature,
-    };
+  // Has to match Sensors order. FIXME: Somehow static
+  static constexpr std::array<Channel, 3> sequence{
+      Channel::InternalReference,
+      Board::CurSenseChan,
+      Channel::Temperature,
+  };
 
-    /**
-     * @brief Initialize, connect, configure and start free running ADC1
-     *
-     */
-    static void init() {
+  /**
+   * @brief Initialize, connect, configure and start free running ADC1
+   *
+   */
+  static void init() {
 #ifdef PROTO_DEBUG_ADC
-        MODM_LOG_INFO << "AdcSampler::init" << modm::endl
-            << modm::flush;
+    MODM_LOG_INFO << "AdcSampler::init" << modm::endl << modm::flush;
 #endif
-        // Target frequency consideration:
-        // Let's get: 4 Channels * 160.5 sampling rate * 32 Oversamples every about 20ms (50Hz) = 1.03 MHz
-        initialize<Board::SystemClock, ClockMode::Asynchronous, 750_kHz>();  // Frequency is slower but more accurate
-        //connect<Board::AdcCurSense::In2>();
-#ifdef HW_V1
-    connect<Board::CurSense::In2>();
-#else
-    connect<Board::CurSense::In3>();
-#endif
-        setSampleTime(SampleTime::Cycles160_5);
-        setResolution(ADC_RESOLUTION);
-        setRightAdjustResult();
-        enableOversampling(OversampleRatio::x32, OversampleShift::Div32);
-        setChannels(sequence);
-        enableInterruptVector(15);
-        enableInterrupt(Interrupt::EndOfConversion);
-        AdcInterrupt1::attachInterruptHandler(sequence_handler);
-        enableFreeRunningMode();
-        startConversion();
-    };
+    // Target frequency consideration:
+    // Let's get: 4 Channels * 160.5 sampling rate * 32 Oversamples every about 20ms (50Hz) = 1.03 MHz
+    initialize<Board::SystemClock, ClockMode::Asynchronous, 750_kHz>();  // Frequency is slower but more accurate
+    connect<Board::CurSenseAdc>();  // Hardware version independent (defined in board.hpp)
+    setSampleTime(SampleTime::Cycles160_5);
+    setResolution(ADC_RESOLUTION);
+    setRightAdjustResult();
+    enableOversampling(OversampleRatio::x32, OversampleShift::Div32);
+    setChannels(sequence);
+    enableInterruptVector(15);
+    enableInterrupt(Interrupt::EndOfConversion);
+    AdcInterrupt1::attachInterruptHandler(sequence_handler);
+    enableFreeRunningMode();
+    startConversion();
+  };
 
-    static void disable() {
-        disableInterrupt(Interrupt::EndOfConversion);
-        disableFreeRunningMode();
-        stopConversion();
-    }
+  static void disable() {
+    disableInterrupt(Interrupt::EndOfConversion);
+    disableFreeRunningMode();
+    stopConversion();
+  }
 
-    /**
-     * @brief Get internal measured VRef
-     *
-     * @return float
-     */
-    static float getInternalVref_f() {
-        return (float)(VDDA_CAL * uint32_t(*VREFINT_CAL)) / _data.at(Sensors::VRef) / 1000.0f;
-    }
+  /**
+   * @brief Get internal measured VRef
+   *
+   * @return float
+   */
+  static float getInternalVref_f() {
+    return (float)(VDDA_CAL * uint32_t(*VREFINT_CAL)) / _data.at(Sensors::VRef) / 1000.0f;
+  }
 
-    /**
-     * @brief Get internal measured VRef
-     *
-     * @return uint16_t
-     */
-    static uint16_t getInternalVref_u() {
-        return (VDDA_CAL * uint32_t(*VREFINT_CAL)) / _data.at(Sensors::VRef);
-    }
+  /**
+   * @brief Get internal measured VRef
+   *
+   * @return uint16_t
+   */
+  static uint16_t getInternalVref_u() {
+    return (VDDA_CAL * uint32_t(*VREFINT_CAL)) / _data.at(Sensors::VRef);
+  }
 
-    /**
-     * @brief Get internal measured junction temp
-     *
-     * @return uint16_t
-     */
-    static uint16_t getInternalTemp() {
-        const int32_t value = ((_data.at(Sensors::Temp) * getInternalVref_u() / VDDA_CAL) - int32_t(*TS_CAL1)) * 1000;
-        return (value / TS_AVG_SLOPE) + TS_CAL1_TEMP;
-    }
+  /**
+   * @brief Get internal measured junction temp
+   *
+   * @return uint16_t
+   */
+  static uint16_t getInternalTemp() {
+    const int32_t value = ((_data.at(Sensors::Temp) * getInternalVref_u() / VDDA_CAL) - int32_t(*TS_CAL1)) * 1000;
+    return (value / TS_AVG_SLOPE) + TS_CAL1_TEMP;
+  }
 
-    /**
-     * @brief Get the voltage of the given ADC sensor.
-     * Attention: For the internal VRef and Temp sensors, see getInternal...() functions.
-     *
-     * @param sensor
-     * @return float
-     */
-    static float getVoltage(const Sensors sensor) {
-        return (getInternalVref_f() * _data.at(sensor)) / ADC_NUM_CODES;
-    }
+  /**
+   * @brief Get the voltage of the given ADC sensor.
+   * Attention: For the internal VRef and Temp sensors, see getInternal...() functions.
+   *
+   * @param sensor
+   * @return float
+   */
+  static float getVoltage(const Sensors sensor) {
+    return (getInternalVref_f() * _data.at(sensor)) / ADC_NUM_CODES;
+  }
 
-private:
-    static std::array<uint16_t, sequence.size()> _data;  // ADC data buffer indexed in the order of sequence
+ private:
+  static std::array<uint16_t, sequence.size()> _data;  // ADC data buffer indexed in the order of sequence
 
-    static void sequence_handler() {
-        static uint8_t seq_idx = 0;
-        auto flag = getInterruptFlags();
+  static void sequence_handler() {
+    static uint8_t seq_idx = 0;
+    auto flag = getInterruptFlags();
 
-        /*// Debug ADC IRQ Flags
-        if (flag & InterruptFlag::EndOfConversion)
-            MODM_LOG_DEBUG << "C";
+    /*// Debug ADC IRQ Flags
+    if (flag & InterruptFlag::EndOfConversion)
+        MODM_LOG_DEBUG << "C";
 
-        if (flag & InterruptFlag::EndOfSequence)
-            MODM_LOG_DEBUG << "S";
+    if (flag & InterruptFlag::EndOfSequence)
+        MODM_LOG_DEBUG << "S";
 
-        if (flag & InterruptFlag::Overrun)
-            MODM_LOG_DEBUG << "O";
+    if (flag & InterruptFlag::Overrun)
+        MODM_LOG_DEBUG << "O";
 
-        MODM_LOG_DEBUG << ", ";*/
+    MODM_LOG_DEBUG << ", ";*/
 
-        if (flag & InterruptFlag::EndOfConversion) {
-            acknowledgeInterruptFlags(InterruptFlag::EndOfConversion);  // Always ack EOC
-            if (!(flag & InterruptFlag::Overrun)) {                     // In the case of an OVR, we can't trust our seq_idx
-                if (seq_idx < sequence.size()) {                        // Ensure array bounds
-                    _data.at(seq_idx) = Adc1::getValue();
-                    seq_idx++;
-                }
-            }
+    if (flag & InterruptFlag::EndOfConversion) {
+      acknowledgeInterruptFlags(InterruptFlag::EndOfConversion);  // Always ack EOC
+      if (!(flag & InterruptFlag::Overrun)) {                     // In the case of an OVR, we can't trust our seq_idx
+        if (seq_idx < sequence.size()) {                          // Ensure array bounds
+          _data.at(seq_idx) = Adc1::getValue();
+          seq_idx++;
         }
-
-        if (flag & InterruptFlag::EndOfSequence) {
-            acknowledgeInterruptFlags(InterruptFlag::EndOfSequence | InterruptFlag::Overrun);  // Also (and earliest) ack a possible overrun
-            seq_idx = 0;
-        }
+      }
     }
+
+    if (flag & InterruptFlag::EndOfSequence) {
+      acknowledgeInterruptFlags(InterruptFlag::EndOfSequence |
+                                InterruptFlag::Overrun);  // Also (and earliest) ack a possible overrun
+      seq_idx = 0;
+    }
+  }
 };
--- firmware/src/board.hpp
+++ firmware/src/board.hpp
@@ -4,7 +4,8 @@
 //
 // This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 //
-// Feel free to use the design in your private/educational projects, but don't try to sell the design or products based on it without getting my consent first.
+// Feel free to use the design in your private/educational projects, but don't try to sell the design or products based
+// on it without getting my consent first.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -29,160 +30,159 @@
 using namespace modm::platform;
 
 namespace Board {
-    /// @ingroup modm_board_xescyfr4
-    /// @{
-    using namespace modm::literals;
+/// @ingroup modm_board_xescyfr4
+/// @{
+using namespace modm::literals;
 
-    struct SystemClock {
-        static constexpr uint32_t Frequency = 48_MHz;  // 48MHz generated from internal RC
-        static constexpr uint32_t Ahb = Frequency;
-        static constexpr uint32_t Apb = Frequency;
+struct SystemClock {
+  static constexpr uint32_t Frequency = 48_MHz;  // 48MHz generated from internal RC
+  static constexpr uint32_t Ahb = Frequency;
+  static constexpr uint32_t Apb = Frequency;
 
-        static constexpr uint32_t Adc1 = Frequency;
-        static constexpr uint32_t Crc = Ahb;
-        static constexpr uint32_t Flash = Ahb;
-        static constexpr uint32_t Exti = Ahb;
-        static constexpr uint32_t Rcc = Ahb;
-        static constexpr uint32_t Timer1 = Apb;
-        static constexpr uint32_t Timer3 = Apb;
-        static constexpr uint32_t Timer14 = Apb;
-        static constexpr uint32_t Timer16 = Apb;
-        static constexpr uint32_t Timer17 = Apb;
-        static constexpr uint32_t Usart1 = Ahb;
-        static constexpr uint32_t Usart2 = Ahb;
+  static constexpr uint32_t Adc1 = Frequency;
+  static constexpr uint32_t Crc = Ahb;
+  static constexpr uint32_t Flash = Ahb;
+  static constexpr uint32_t Exti = Ahb;
+  static constexpr uint32_t Rcc = Ahb;
+  static constexpr uint32_t Timer1 = Apb;
+  static constexpr uint32_t Timer3 = Apb;
+  static constexpr uint32_t Timer14 = Apb;
+  static constexpr uint32_t Timer16 = Apb;
+  static constexpr uint32_t Timer17 = Apb;
+  static constexpr uint32_t Usart1 = Ahb;
+  static constexpr uint32_t Usart2 = Ahb;
 
-        static bool inline enable() {
-            Rcc::enableInternalClock();                       // 48MHz generated from internal RC
-            Rcc::setHsiSysDivider(Rcc::HsiSysDivider::Div1);  // = 48MHz HSISYS
-            Rcc::setFlashLatency<Frequency>();
-            Rcc::setAhbPrescaler(Rcc::AhbPrescaler::Div1);  // = 48MHz HCLK
-            Rcc::setApbPrescaler(Rcc::ApbPrescaler::Div1);  // = 48MHz PCLK/APB Timer Clocks
-            Rcc::updateCoreFrequency<Frequency>();          // update frequencies for busy-wait delay functions
-            return true;
-        }
-    };
+  static bool inline enable() {
+    Rcc::enableInternalClock();                       // 48MHz generated from internal RC
+    Rcc::setHsiSysDivider(Rcc::HsiSysDivider::Div1);  // = 48MHz HSISYS
+    Rcc::setFlashLatency<Frequency>();
+    Rcc::setAhbPrescaler(Rcc::AhbPrescaler::Div1);  // = 48MHz HCLK
+    Rcc::setApbPrescaler(Rcc::ApbPrescaler::Div1);  // = 48MHz PCLK/APB Timer Clocks
+    Rcc::updateCoreFrequency<Frequency>();          // update frequencies for busy-wait delay functions
+    return true;
+  }
+};
 
-    using LedGreen = GpioC15;
+using LedGreen = GpioC15;
 
-    // Hardware version specific LEDs (always define both for cross-version error indication)
-    using LedRed_v1 = GpioC14;
-    using LedRed_v2 = GpioB6;
+// Hardware version specific LEDs (always define both for cross-version error indication)
+using LedRed_v1 = GpioC14;
+using LedRed_v2 = GpioB6;
 
-    // Alias for the correct red LED based on compiled hardware version
+// Alias for the correct red LED based on compiled hardware version
 #ifdef HW_V1
-    using LedRed = LedRed_v1;
+using LedRed = LedRed_v1;
 #else
-    using LedRed = LedRed_v2;
+using LedRed = LedRed_v2;
 #endif
-    /// @}
+/// @}
 
-    namespace host {
-        /// @ingroup modm_board_xescyfr4
-        /// @{
-        using Tx = GpioOutputA9;
-        using Rx = GpioInputA10;
-        using Uart = BufferedUart<UsartHal1, UartTxBuffer<32>, UartRxBuffer<32>>;
+namespace host {
+/// @ingroup modm_board_xescyfr4
+/// @{
+using Tx = GpioOutputA9;
+using Rx = GpioInputA10;
+using Uart = BufferedUart<UsartHal1, UartTxBuffer<32>, UartRxBuffer<32>>;
 #ifdef HW_V1
-        using Shutdown = GpioInputA8;
+using Shutdown = GpioInputA8;
 #else
-        using Shutdown = GpioInputC14;
+using Shutdown = GpioInputC14;
 #endif
-        /// @}
-    } // namespace host
+/// @}
+}  // namespace host
 
-    namespace proto_uart {
-        /// @ingroup modm_board_xescyfr4
-        /// @{
-        using Tx = GpioOutputA4;
-        using Rx = GpioInputA5;
-        using Uart = BufferedUart<UsartHal2, UartTxBuffer<32>>;
-        /// @}
-    } // namespace proto_uart
+namespace proto_uart {
+/// @ingroup modm_board_xescyfr4
+/// @{
+using Tx = GpioOutputA4;
+using Rx = GpioInputA5;
+using Uart = BufferedUart<UsartHal2, UartTxBuffer<32>>;
+/// @}
+}  // namespace proto_uart
 
-    // VM-Switch
-    namespace vm_switch {
-        /// @ingroup modm_board_xescyfr4
-        /// @{
+// VM-Switch
+namespace vm_switch {
+/// @ingroup modm_board_xescyfr4
+/// @{
 #ifdef HW_V1
-        using In = GpioOutputA0;         // IN (High = VMC-on)
-        using DiagEnable = GpioOutputB6; // DIAG_EN (High = Diagnostics on)
-        using Fault = GpioInputF2;       // !FAULT (Low = Fault detected)
+using In = GpioOutputA0;          // IN (High = VMC-on)
+using DiagEnable = GpioOutputB6;  // DIAG_EN (High = Diagnostics on)
+using Fault = GpioInputF2;        // !FAULT (Low = Fault detected)
 #else
-        using In = GpioOutputA1;         // IN (High = VMC-on)
-        using DiagEnable = GpioOutputA2; // DIAG_EN (High = Diagnostics on)
+using In = GpioOutputA1;          // IN (High = VMC-on)
+using DiagEnable = GpioOutputA2;  // DIAG_EN (High = Diagnostics on)
 #endif
-        /// @}
-    } // namespace vm_switch
+/// @}
+}  // namespace vm_switch
 
-    // Motor
-    namespace motor {
-        /// @ingroup modm_board_xescyfr4
-        /// @{
+// Motor
+namespace motor {
+/// @ingroup modm_board_xescyfr4
+/// @{
 #ifdef HW_V1
-        using SA = GpioInputB7;    // Motor SA (Hall)
-        using SATimChan = SA::Ch4; // SA timer channel
-        static constexpr auto SACaptureInterrupt = Timer1::Interrupt::CaptureCompare4; // SA timer capture/compare interrupt
-        static constexpr auto SACaptureFlag = Timer1::InterruptFlag::CaptureCompare4;   // SA timer capture/compare flag
-        using Brk = GpioOutputA3;  // Motor BRK
-        using RS = GpioOutputA6;   // Motor !RS (Rapid/Rotor Start) (LowActive)
+using SA = GpioInputB7;                                                         // Motor SA (Hall)
+using SATimChan = SA::Ch4;                                                      // SA timer channel
+static constexpr auto SACaptureInterrupt = Timer1::Interrupt::CaptureCompare4;  // SA timer capture/compare interrupt
+static constexpr auto SACaptureFlag = Timer1::InterruptFlag::CaptureCompare4;   // SA timer capture/compare flag
+using Brk = GpioOutputA3;                                                       // Motor BRK
+using RS = GpioOutputA6;  // Motor !RS (Rapid/Rotor Start) (LowActive)
 #else
-        using SA = GpioInputA0;    // Motor SA (Hall)
-        using SATimChan = SA::Ch1; // SA timer channel
-        static constexpr auto SACaptureInterrupt = Timer1::Interrupt::CaptureCompare1; // SA timer capture/compare interrupt
-        static constexpr auto SACaptureFlag = Timer1::InterruptFlag::CaptureCompare1;   // SA timer capture/compare flag
-        using Brk = GpioOutputA6;  // Motor BRK
-        using RS = GpioOutputA8;   // Motor !RS (Rapid/Rotor Start) (LowActive)
+using SA = GpioInputA0;                                                         // Motor SA (Hall)
+using SATimChan = SA::Ch1;                                                      // SA timer channel
+static constexpr auto SACaptureInterrupt = Timer1::Interrupt::CaptureCompare1;  // SA timer capture/compare interrupt
+static constexpr auto SACaptureFlag = Timer1::InterruptFlag::CaptureCompare1;   // SA timer capture/compare flag
+using Brk = GpioOutputA6;                                                       // Motor BRK
+using RS = GpioOutputA8;  // Motor !RS (Rapid/Rotor Start) (LowActive)
 #endif
-        /// @}
-    } // namespace motor
+/// @}
+}  // namespace motor
 
-    /// @ingroup modm_board_xescyfr4
-    /// @{
+/// @ingroup modm_board_xescyfr4
+/// @{
 #ifdef HW_V1
-    static constexpr auto CurSenseChan = Adc1::Channel::In2;
-    using CurSense = GpioInputA2;
+static constexpr auto CurSenseChan = Adc1::Channel::In2;
+using CurSense = GpioInputA2;
+using CurSenseAdc = CurSense::In2;  // ADC signal type for connect<>
 #else
-    static constexpr auto CurSenseChan = Adc1::Channel::In3;
-    using CurSense = GpioInputA3;
+static constexpr auto CurSenseChan = Adc1::Channel::In3;
+using CurSense = GpioInputA3;
+using CurSenseAdc = CurSense::In3;  // ADC signal type for connect<>
 #endif
 
 #ifdef PROTO_DEBUG
-    // Forward declaration - definition in board.cpp
-    extern modm::IODeviceWrapper<proto_uart::Uart, modm::IOBuffer::BlockIfFull> LoggerDevice;
+// Forward declaration - definition in board.cpp
+extern modm::IODeviceWrapper<proto_uart::Uart, modm::IOBuffer::BlockIfFull> LoggerDevice;
 #endif
 
-    inline void
-        initialize() {
-        SystemClock::enable();
-        SysTickTimer::initialize<SystemClock>();
+inline void initialize() {
+  SystemClock::enable();
+  SysTickTimer::initialize<SystemClock>();
 
-        // Remap and init host uart
-        GpioA9::remap();  // Remap A9 -> A11
-        GpioA10::remap(); // Remap A10 -> A12
-        host::Uart::connect<host::Tx::Tx, host::Rx::Rx>();
-        host::Uart::initialize<SystemClock, 115200_Bd>();
-        host::Shutdown::setInput(Gpio::InputType::Floating);
+  // Remap and init host uart
+  GpioA9::remap();   // Remap A9 -> A11
+  GpioA10::remap();  // Remap A10 -> A12
+  host::Uart::connect<host::Tx::Tx, host::Rx::Rx>();
+  host::Uart::initialize<SystemClock, 115200_Bd>();
+  host::Shutdown::setInput(Gpio::InputType::Floating);
 
 #ifdef PROTO_DEBUG
-        proto_uart::Uart::connect<proto_uart::Tx::Tx, proto_uart::Rx::Rx>();
-        proto_uart::Uart::initialize<SystemClock, PROTO_DEBUG_BAUD>();
-        MODM_LOG_INFO << modm::endl
-            << "Board initialized" << modm::endl
-            << modm::flush;
+  proto_uart::Uart::connect<proto_uart::Tx::Tx, proto_uart::Rx::Rx>();
+  proto_uart::Uart::initialize<SystemClock, PROTO_DEBUG_BAUD>();
+  MODM_LOG_INFO << modm::endl << "Board initialized" << modm::endl << modm::flush;
 #endif
-    }
-    /// @}
+}
+/// @}
 
-} // namespace Board
+}  // namespace Board
 
 #ifdef PROTO_DEBUG
 // Forward declarations for logger streams (definitions in board.cpp)
 namespace modm::log {
-    extern Logger debug;
-    extern Logger info;
-    extern Logger warning;
-    extern Logger error;
-}
+extern Logger debug;
+extern Logger info;
+extern Logger warning;
+extern Logger error;
+}  // namespace modm::log
 #endif
 
-#endif // MODM_XESCYFR4_BOARD_HPP
+#endif  // MODM_XESCYFR4_BOARD_HPP
